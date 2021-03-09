1c0fd901e8ede0eda5a394284bd9416bc7541729
karinakostenko <79445871+karinakostenko@users.noreply.github.com>
2021-03-09 12:19:41 +0200

    Update 10.b1_skolenu_darbi.py
    Rindā Nr.22 pievienoju matemātisku darbību sin30+0.5

--- 10.b1_skolenu_darbi.py
+++ 10.b1_skolenu_darbi.py
@@ -6,22 +6,22 @@ Tatjana Grencione
 
 3 Fedaks Alekss
 
-4 Gaile Aïona
+4 Gaile AÃ¯ona
 
 5 Gaile Anna
 
-6 Griğaèova Violeta
+6 GriÃ°aÃ¨ova Violeta
 
-7 Jakovïevs Aleksandrs
+7 JakovÃ¯evs Aleksandrs
 
-8 Jeroğèenkova Alîna
+8 JeroÃ°Ã¨enkova AlÃ®na
 
 9 Kopilovs Marks
 
-10 Kostenko Karîna
+10 Kostenko KarÃ®na
+ print(sin(radians(30)+fmod(13,2)))
+11 KuzÃ²ecovs Ivans
 
-11 Kuzòecovs Ivans
-
-12 Ïetko Jevgçnijs
+12 Ãetko JevgÃ§nijs
 
 13 Skuridins Artis Daniels
